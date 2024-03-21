eafd7ee0ca7dba59c24dd0d0a24ec07fd74f3e49
Ashton <119149634+TheParadiddle@users.noreply.github.com>
2024-03-20 19:57:57 -0500

    Add cluster map to index page

--- models/campground.js
+++ models/campground.js
@@ -3,13 +3,16 @@ const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-        url: String,
-        filename: String
+    url: String,
+    filename: String
 });
 
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -37,6 +40,12 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 50)}...</p>`
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
